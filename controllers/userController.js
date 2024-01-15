4c1d82ccdc3b47ddab28aabef581156089dba284
FahadBinBashar <fuadbinbashar1990@gmail.com>
2024-01-16 01:30:05 +0600

    Bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/userModel');
+const bcrypt = require("bcrypt");
 
 function getAllUsers(req, res) {
   userModel.getAllUsers((err, users) => {
@@ -10,17 +11,20 @@ function getAllUsers(req, res) {
   });
 }
 
-function createUser(req, res){
-    
-    const body = req.body;
-    console.log('Received request body:', body);
-    userModel.createUser(body, (err, result) =>{
-        if(err){
-          res.status(500).json({ error: err.message }); 
-        }else{
-          res.status(201).json({ message: 'User registered successfully', userId: result });
-        }
-    });
+const createUser=async(req, res)=>{
+  const body = req.body;
+  const { password } = body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const saveData = {...body, password:hashPassword}
+
+  console.log('Received request body:', saveData,hashPassword);
+  userModel.createUser(saveData, (err, result) =>{
+      if(err){
+        res.status(500).json({ error: err.message }); 
+      }else{
+        res.status(201).json({ message: 'User registered successfully', userId: result });
+      }
+  });
 }
 
 
